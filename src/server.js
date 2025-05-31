ba7c50902890afe0c6c6a854a601749905ff792d
Ziburk <ziburk76@gmail.com>
2025-05-31 16:29:03 +0300

    Исправлены последние проблемы
    Осталось чуть подкорерктировать порядок задач

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
     origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
